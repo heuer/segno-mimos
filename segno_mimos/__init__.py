30a4a451804676809991c3adfed3a93324d9075b
Lars <heuer@semagia.com>
2016-08-17 12:37:53 +0200

    Incremented version

--- segno_mimos/__init__.py
+++ segno_mimos/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2015 -- Lars Heuer - Semagia <http://www.semagia.com/>.
+# Copyright (c) 2016 -- Lars Heuer - Semagia <http://www.semagia.com/>.
 # All rights reserved.
 #
 # License: BSD License
 #
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
