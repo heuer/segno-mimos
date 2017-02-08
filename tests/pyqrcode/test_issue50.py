a2da2779ba3017dbe229a6993da9e9297609dd7d
Lars <heuer@semagia.com>
2017-02-08 11:29:52 +0100

    Updated test case to work with 3.4 and 3.6

--- tests/pyqrcode/test_issue50.py
+++ tests/pyqrcode/test_issue50.py
@@ -18,10 +18,10 @@ class FakeString(str):
     def __new__(cls, *more):
         return str.__new__(cls, *more)
 
-    def encode(self, encoding=None, errors=None):
+    def encode(self, encoding=None, errors='strict'):
         if encoding == 'shiftjis':
             raise LookupError("unknown encoding: shiftjis")
-        return self.encode(encoding, errors)
+        return super(FakeString, self).encode(encoding, errors)
 
 
 def test_constructing_without_shiftjis_encoding_available():
