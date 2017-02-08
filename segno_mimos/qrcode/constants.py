8c8d99d9f81e6325f26888a2faa82800c204abe7
Lars <heuer@semagia.com>
2017-02-08 11:30:25 +0100

    Updated python-qrcode impl

--- segno_mimos/qrcode/constants.py
+++ segno_mimos/qrcode/constants.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2011, Lincoln Loop
-# Copyright (c) 2016 -- Lars Heuer - Semagia <http://www.semagia.com/>.
+# Copyright (c) 2011 - 2017, Lincoln Loop
+# Copyright (c) 2016 - 2017 -- Lars Heuer - Semagia <http://www.semagia.com/>.
 # All rights reserved.
 #
 # License: BSD License
